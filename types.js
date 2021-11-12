a595e2f1cdc28010a8c45aedd24eaa81b88dd353
Jahnavi412 <82147813+Jahnavi412@users.noreply.github.com>
2021-11-12 17:54:08 +0530

    declarining datatypes

---
+++ types.js
@@ -0,0 +1,48 @@
+// primitive types
+let name = 'jahnavi';
+let age =23;
+let isApporved = true;
+let painocolor = null;
+console.log(name,age,isApporved,painocolor);
+console.log(name + " type=" +typeof name);
+console.log(age + " type=" +  typeof age);
+
+
+//Reference types(objects,arrays,functions)
+// object creating
+let person ={
+    name : 'aaaa',
+    age: 20
+
+};
+
+// dynamically we can change properties of the object by two ways
+// Dot notation
+person.name = 'jhon';
+
+
+// Bracket notation
+person['age'] = 22;
+console.log(person);
+
+
+//arrays
+let seletecedcolors=['red','blue'];
+seletecedcolors[2]='green';
+console.log(seletecedcolors.length);
+
+
+//functions
+// performing an task
+function greet(name,lastname){
+    console.log('hello '  +  name + ' ' + lastname );
+
+}
+greet('jhon','smith');
+
+
+// calculating an value
+function square(number){
+    return number*number;
+}
+console.log(square(2));
