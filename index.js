5376a48809ab17ef3cf1aa2ae23d58c1e9cc3483
Jahnavi412 <82147813+Jahnavi412@users.noreply.github.com>
2021-11-12 17:53:35 +0530

    declarining variables

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('hello world1');
+
+// declaring the variables in JAVA SCRIPT
+// rules
+// names should be meaningful ,no keywords are used, no spaces or symbols given,and they are case senstive
+
+let variable ='java sciprt';
+console.log('a', variable);
+
+// declaring constants in js can be done by using const keyword
+const interest = 1;
+console.log(interest);
