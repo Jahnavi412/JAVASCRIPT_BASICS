5cfb32b1c81c1e3eab6705b14ad023f10dd63c80
Jahnavi412 <82147813+Jahnavi412@users.noreply.github.com>
2021-11-12 19:05:38 +0530

    using operators and functions

---
+++ operators.js
@@ -0,0 +1,21 @@
+let add = 2+10;
+let sub = 10-5;
+let div = 10%5;
+let mul = 10*2;
+
+console .log( add,sub,div,mul);
+
+// using functions
+function addnumbers(n1, n2){
+console.log(n1+n2);
+//another method of using functions
+let result = n1-n2;
+return result;
+
+}
+let result = addnumbers(100,200);
+console.log(result);
+
+
+
+
